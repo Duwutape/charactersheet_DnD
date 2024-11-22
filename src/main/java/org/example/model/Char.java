3db582efd48b3f93d2171692caed120ace860375
duwutape <raven.langheld@gmx.de>
2024-11-22 02:02:03 +0100

    started implementing game classes

---
+++ src/main/java/org/example/model/Char.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import org.example.gameclass.GameClass;
+
+public class Char {
+    private String name;
+    private GameClass gameClass;
+    private Species species;
+    private int level;
+
+}
