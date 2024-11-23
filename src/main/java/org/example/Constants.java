4d613e300ecaff3c365339faf7e81d9036fe87d2
duwutape <raven.langheld@gmx.de>
2024-11-23 14:11:29 +0100

    added constants for dice and ability names

--- src/main/java/org/example/Constants.java
+++ src/main/java/org/example/Constants.java
@@ -25,4 +25,27 @@ public class Constants {
     public static final String WIZARD = "Wizard";
 
     // SPECIES NAMES
+
+    // ABILITIES
+    public static final String STRENGTH = "Strength";
+    public static final String DEXTERITY = "Dexterity";
+    public static final String CONSTITUTION = "Constitution";
+    public static final String INTELLIGENCE = "Intelligence";
+    public static final String WISDOM = "Wisdom";
+    public static final String CHARISMA = "Charisma";
+
+    public static final String STR = "Str";
+    public static final String DEX = "Dex";
+    public static final String CON = "Con";
+    public static final String INT = "Int";
+    public static final String WIS = "Wis";
+    public static final String CHA = "Cha";
+
+    //DICE
+    public static final String D4 = "D4";
+    public static final String D6 = "D6";
+    public static final String D8 = "D8";
+    public static final String D10 = "D10";
+    public static final String D12 = "D12";
+    public static final String D20 = "D20";
 }
