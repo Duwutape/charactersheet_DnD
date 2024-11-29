8e1b8db6d57d0bdc06a0c0af85e6d5989a3087f7
duwutape <raven.langheld@gmx.de>
2024-11-29 01:41:20 +0100

    removed amountBardicInspiration because it is 2024 content

--- src/main/java/org/example/classes/Bard.java
+++ src/main/java/org/example/classes/Bard.java
@@ -1,24 +1,10 @@
 package org.example.classes;
 
 import org.example.model.GameClass;
-import org.example.model.Spell;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Bard extends GameClass {
 
-    private String amountBardicInspiration;
-
     public Bard(int level) {
         super(level);
-    }
-
-    public String getAmountBardicInspiration() {
-        return amountBardicInspiration;
-    }
-
-    public void setAmountBardicInspiration(String amountBardicInspiration) {
-        this.amountBardicInspiration = amountBardicInspiration;
     }
 }
